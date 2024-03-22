7bdbe439f1e3373ee2083b98ba22b9c03c13340f
Andrew Eisenberg <aeisenberg@github.com>
2024-03-21 17:04:33 -0700

    Run test workflow on pushes to main

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 A simple GitHub Action to post to Mastodon.
 
+## First get an Application Token
+
+1. Log in to the Mastodon account you want to post from.
+2. Open the settings page.
+3. Go to the development tab.
+4. Click "New Application"
+5. The application needs `write:statuses` permissions and nothing else.
+6. Copy the Access Token. Keep it private and add it as a secret to the repository using this action.
+
 ## To use this action
 
 ```
--- post.sh
+++ post.sh
@@ -12,6 +12,9 @@ status_message=$3
 # Visibility of the status message
 visibility=$4
 
+# Escape single and double quotes in the status message
+status_message=$(echo $status_message | sed "s/'/\\\\'/g" | sed 's/"/\\"/g')
+
 # fail if any parameter is empty
 if [ -z "$instance_url" ] || [ -z "$access_token" ] || [ -z "$status_message" ] || [ -z "$visibility" ]; then
   echo "Usage: $0 <instance_url> <access_token> <status_message> <visibility>"
@@ -21,9 +24,18 @@ fi
 echo "Posting status message: '$status_message' to '$instance_url' with visibility '$visibility'."
 
 # Send the POST request
-curl -X POST -Ss \
+http_response="$(curl -X POST -Ss -w "%{response_code}" -o result.json \
+  -H "Idempotency-Key: $(date +%s)" \
   -H "Authorization: Bearer $access_token" \
   -H "Content-Type: application/json" \
-  -H "Idempotency-Key: $(date +%s)" \
   -d "{\"status\":\"$status_message\",\"visibility\":\"$visibility\"}" \
-  "$instance_url/api/v1/statuses"
+  "$instance_url/api/v1/statuses")"
+
+if [ $http_response != "200" ]; then
+    echo "Error: HTTP response code $http_response"
+    cat result.json
+    exit 1
+else
+    echo "Success! $http_response"
+    cat result.json
+fi
